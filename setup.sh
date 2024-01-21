91cad531e3dca4feda78c9197e6491861deebd59
Ryan Barouki (local) <Ryan>
2024-01-21 13:34:34 +0000

    update symlinks in setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 ln -s $HOME/.config/nvim/polybar $HOME/.config/polybar
+ln -s $HOME/.config/nvim/starship.toml $HOME/.config/starship.toml
+ln -s $HOME/.config/nvim/.bashrc $HOME/.bashrc
