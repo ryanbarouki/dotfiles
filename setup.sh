57962d92592308d4e048947183b324fece87eb71
Ryan Barouki (local) <Ryan>
2024-01-21 14:29:13 +0000

    add stuff

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Make symbolic links so that relevant programs find the config files
 ln -s $HOME/.config/nvim/polybar $HOME/.config/polybar
 ln -s $HOME/.config/nvim/starship.toml $HOME/.config/starship.toml
 ln -s $HOME/.config/nvim/.bashrc $HOME/.bashrc
+ln -s $HOME/.config/nvim/i3 $HOME/.config/i3
